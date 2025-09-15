4a483c870394f9ef86077729f1e2cba3602d9896
FabianGhioca <ghiocafabian2004@gmail.com>
2025-09-15 12:36:44 +0200

    15/09/25

--- CorsoAngularZone/src/app/profile/profile.ts
+++ CorsoAngularZone/src/app/profile/profile.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TemplateDrivenForms } from "../views/forms/template-driven-forms/template-driven-forms";
+import { ReactiveForms } from "../views/forms/reactive-forms/reactive-forms";
 
 @Component({
   selector: 'app-profile',
-  imports: [TemplateDrivenForms],
+  imports: [TemplateDrivenForms, ReactiveForms],
   templateUrl: './profile.html',
   styleUrl: './profile.scss'
 })
--- CorsoAngularZone/src/app/views/forms/reactive-forms/reactive-forms.ts
+++ CorsoAngularZone/src/app/views/forms/reactive-forms/reactive-forms.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -7,12 +7,37 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './reactive-forms.html',
   styleUrls: ['./reactive-forms.scss']
 })
 export class ReactiveForms {
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: new FormControl<string>("", [Validators.required, Validators.maxLength(20)]),
+      cognome: new FormControl<string>("", Validators.required),
+      password: new FormControl<string>("", [Validators.minLength(8), Validators.required]),
+      ConfermaPassword: new FormControl<string>("", [Validators.minLength(8), Validators.required])
+    }, {
+      Validators: this.matchPasswordValidator
+    });
+  }
+
+  matchPasswordValidator(form: AbstractControl){
+    const password = form.get("password")?.value;
+    const ConfermaPassword = form.get("ConfermaPassword")?.value;
+
+    return password === ConfermaPassword ? null : { mismatch: true }
+  }
+
+
   submitForm() {
-    console.log("Form submit");
+    if(this.form.valid){
+      console.log(this.form.value);
+    } else {
+      alert("Form non valido");
+    }
+    console.log(this.form.value);
   }
 }
